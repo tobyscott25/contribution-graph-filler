df4bcd56078154a07e1be1167210d5705fbdc12d
Toby Scott <hi@tobyscott.dev>
2023-05-16 22:11:08 +1000

    wrap in for loop

--- main.go
+++ main.go
@@ -48,41 +48,43 @@ func main() {
 	daysBetween := dates.DaysBetween(joinDate, time.Now())
 	fmt.Println("Will create " + strconv.Itoa(daysBetween) + " dummy commits.")
 
-	files.EditDummyCommitFile(dummyCommitFilePath, 1)
+	for i := 0; i < daysBetween; i++ {
 
-	_, err = workTree.Add(dummyCommitFileName)
-	if err != nil {
-		fmt.Println("Error staging file:", err)
-		return
+		files.EditDummyCommitFile(dummyCommitFilePath, i)
+
+		_, err = workTree.Add(dummyCommitFileName)
+		if err != nil {
+			fmt.Println("Error staging file:", err)
+			return
+		}
+
+		status, err := workTree.Status()
+		if err != nil {
+			fmt.Println("Error getting the working tree status:", err)
+			return
+		}
+		fmt.Println("Git status:")
+		fmt.Println(status)
+
+		globalGitConfig, _ := config.LoadConfig(1)
+
+		commit, err := workTree.Commit("Synthetic Commit No. "+strconv.Itoa(i), &git.CommitOptions{
+			Author: &object.Signature{
+				Name:  globalGitConfig.User.Name,
+				Email: globalGitConfig.User.Email,
+				When:  time.Now().AddDate(0, 0, -i),
+			},
+		})
+		if err != nil {
+			fmt.Println("Error creating commit:", err)
+			return
+		}
+
+		commitObject, err := repository.CommitObject(commit)
+		if err != nil {
+			fmt.Println("Error getting commit:", err)
+			return
+		}
+		fmt.Println(commitObject)
 	}
-
-	// We can verify the current status of the worktree using the method Status.
-	status, err := workTree.Status()
-	if err != nil {
-		fmt.Println("Error getting the working tree status:", err)
-		return
-	}
-	fmt.Println("Git status:")
-	fmt.Println(status)
-
-	globalGitConfig, _ := config.LoadConfig(1)
-
-	commit, err := workTree.Commit("Synthetic Commit", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  globalGitConfig.User.Name,
-			Email: globalGitConfig.User.Email,
-			When:  time.Now().AddDate(0, 0, -1),
-		},
-	})
-	if err != nil {
-		fmt.Println("Error creating commit:", err)
-		return
-	}
-
-	commitObject, err := repository.CommitObject(commit)
-	if err != nil {
-		fmt.Println("Error getting commit:", err)
-		return
-	}
-	fmt.Println(commitObject)
 }
